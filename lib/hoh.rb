6f6b80aef6548d98d6f1d130ef40f2a990ab1f0e
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-22 23:50:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,10 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
   {
-    
+    PORTION_1 = {
+      label: "Kingdon",
+      sub
+    }
   }
 
 end
