55e3da018dec4a473402cdc9c8490b90d92ffc05
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-23 00:42:50 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,14 +44,21 @@ def naming_system
   
   {
     label: "Kingdom",
-    :sub_category {
+    :sub_category => {
       label: "Phylum",
-      :sub_category {
+      :sub_category => {
         label: "Class",
-        :sub_category {
+        :sub_category => {
           label: "Order",
-          :sub_category {
-            label: "Family"
+          :sub_category => {
+            label: "Family",
+            :sub_category => {
+              label: "Genus",
+              :sub_category => {
+                label: "Species",
+                :sub_category => nil
+              }
+            }
           }
         }
       }
